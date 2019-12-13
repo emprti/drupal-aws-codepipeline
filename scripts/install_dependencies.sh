92bb1d17117dd28730a738b3bd2df347d720feb3
Patterson, Evan <emp@rti.org>
2019-12-13 11:04:46 -0500

    script updates and starting Drupal install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,3 +3,6 @@ if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd24 >&2; fi # install
 if ! [ -x "$(command -v git)" ]; then yum install -y git >&2; fi # install git if not already installed
 if ! [ -x "$(command -v mysql)" ]; then yum install -y mysql56 >&2; fi # install mysql if not already installed
 if ! [ -x "$(command -v php7)" ]; then yum install -y php73 php73-cli php73-common php73-pdo php73-mysqlnd php73-xml php73-gd libssh2 php73-pecl-ssh2 php73-mbstring php73-fpm php73-opcache php73-pecl-memcached >&2; fi # install php and dependencies if not already installed
+
+sed -i 's/allow_url_fopen = On/allow_url_fopen = Off/g' /etc/php.ini
+sed -i 's/expose_php = On/expose_php = Off/g' /etc/php.ini
---
+++ scripts/install_drupal.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+composer create-project drupal-composer/drupal-project:8.x-dev /var/www/drupal --no-interaction
+
+#drush site-install standard --yes \
+#--site-name='We Think Twice Dev Site' \
+#--site-mail=emp@rti.org \
+#--account-name=admin \
+#--account-pass=fee9phah \
+#--db-url=mysql://drupal:quee3cae@dev-drupalstack-1q2kbcf3rjin9-rds-auroradbcluster-18rsaezn9ipeq.cluster-cptbllp3w4ar.us-east-1.rds.amazonaws.com/sradevdrupaldb \
+#--db-prefix=drupal_
+
+rm -f /etc/httpd/conf.d/autoindex.conf /etc/httpd/conf.d/notrace.conf /etc/httpd/conf.d/userdir.conf /etc/httpd/conf.d/welcome.conf
+
+cat <<EOF > /etc/httpd/conf.d/drupal.conf
+<VirtualHost *:80>
+    ServerAdmin emp@rti.org
+    ServerName  projectsrti.org
+    ServerAlias www.projectsrti.org
+    DocumentRoot /var/www/drupal
+    <Directory /var/www/drupal>
+        Options -Indexes -MultiViews +FollowSymLinks
+        AllowOverride All
+        Order allow,deny
+        allow from all
+    </Directory>
+    RewriteEngine On
+    RewriteCond %{HTTP:X-Forwarded-Proto} =http
+    RewriteRule .* https://%{HTTP:Host}%{REQUEST_URI} [L,R=permanent]
+    LogLevel warn
+    ErrorLog /var/log/httpd/drupal-error.log
+    CustomLog /var/log/httpd/drupal-access.log combined
+</VirtualHost>
+ServerSignature Off
+ServerTokens Prod
+EOF
+
+chown -R apache:apache /var/www/drupal/
+chmod -R 750 /var/www/drupal
+chmod -R 770 /var/www/drupal/sites/default/files
+
